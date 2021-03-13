f68b63415ff0f742ed7afab47faace81c2237001
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2021-03-13 11:45:59 +0100

    add tests for modbus component

---
+++ tests/component_tests/modbus/test_config.py
@@ -0,0 +1,31 @@
+"""Tests for the modbus component."""
+
+from esphome import core
+from esphome.components.modbus import CONFIG_SCHEMA
+from esphome.const import CONF_SETUP_PRIORITY
+
+
+def test_empty():
+    config = dict()
+    validated = CONFIG_SCHEMA(config)
+    assert validated['id'].is_declaration == True
+    assert validated['id'].is_manual == False
+    assert validated['uart_id'].is_declaration == False
+    assert validated['uart_id'].is_manual == False
+
+def test_manual_id():
+    config = dict(id='mymodbus')
+    validated = CONFIG_SCHEMA(config)
+    assert validated['id'].is_declaration == True
+    assert validated['id'].is_manual == True
+
+def test_uart_id():
+    config = dict(uart_id='myuart')
+    validated = CONFIG_SCHEMA(config)
+    assert validated['uart_id'].is_declaration == False
+    assert validated['uart_id'].is_manual == True
+
+def test_setup_priority():
+    config = dict(setup_priority=99.9)
+    validated = CONFIG_SCHEMA(config)
+    assert validated['setup_priority'] == 99.9
