9e0c9d2516e121430712717f10034c215f623a3a
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2021-03-14 16:53:36 +0100

    Added addresses to modbus sensor

--- esphome/components/modbus/sensor/__init__.py
+++ esphome/components/modbus/sensor/__init__.py
@@ -1,8 +1,8 @@
+import collections
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart
 from esphome.const import CONF_ID, CONF_ADDRESS
-from esphome.components import sensor, modbus
+from esphome.components import modbus
 from esphome.core import coroutine
 
 AUTO_LOAD = ["modbus"]
@@ -10,18 +10,25 @@ AUTO_LOAD = ["modbus"]
 CONF_MODBUS_ID = "modbus_id"
 CONF_DEVICE_ID = "device_id"
 CONF_FUNCTION = "function"
+CONF_ADDRESSES = "addresses"
+CONF_ADDRESS_TYPE = "address_type"
 
-ModbusFunctionCode = modbus.modbus_ns.enum("ModbusFunctionCode")
-MODBUS_READ_FUNCTION = {
-    "read_coils": ModbusFunctionCode.READ_COILS,
-    "read_discrete_inputs": ModbusFunctionCode.READ_DISCRETE_INPUTS,
-    "read_holding_registers": ModbusFunctionCode.READ_HOLDING_REGISTERS,
-    "read_input_registers": ModbusFunctionCode.READ_INPUT_REGISTERS,
+FunctionCode = modbus.modbus_ns.enum("FunctionCode")
+READ_FUNCTION = {
+    "read_coils": FunctionCode.READ_COILS,
+    "read_discrete_inputs": FunctionCode.READ_DISCRETE_INPUTS,
+    "read_holding_registers": FunctionCode.READ_HOLDING_REGISTERS,
+    "read_input_registers": FunctionCode.READ_INPUT_REGISTERS,
 }
-MODBUS_WRITE_FUNCTION = {
-    "write_single_coil": ModbusFunctionCode.WRITE_SINGLE_COIL,
-    "write_single_register": ModbusFunctionCode.WRITE_SINGLE_REGISTER,
-    "write_multiple_registers": ModbusFunctionCode.WRITE_MULTIPLE_REGISTERS,
+WRITE_FUNCTION = {
+    "write_single_coil": FunctionCode.WRITE_SINGLE_COIL,
+    "write_single_register": FunctionCode.WRITE_SINGLE_REGISTER,
+    "write_multiple_registers": FunctionCode.WRITE_MULTIPLE_REGISTERS,
+}
+AddressType = modbus.modbus_ns.enum("AddressTypeCode")
+ADRESS_TYPE = {
+    "16bit": AddressType.ADDRESS_TYPE_16BIT,
+    "32bit": AddressType.ADDRESS_TYPE_32BIT,
 }
 
 ModbusSensor = modbus.modbus_ns.class_(
@@ -29,16 +36,48 @@ ModbusSensor = modbus.modbus_ns.class_(
 )
 
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(ModbusSensor),
-        cv.GenerateID(CONF_MODBUS_ID): cv.use_id(modbus.Modbus),
-        cv.Optional(CONF_DEVICE_ID, default=1): cv.hex_int_range(min=1, max=247),
-        cv.Optional(CONF_FUNCTION, default="read_input_registers"): cv.enum(
-            MODBUS_READ_FUNCTION
-        ),
-    }
-).extend(cv.polling_component_schema("60s"))
+def validate_addresses(value):
+    addresses = [x[CONF_ADDRESS] for x in value]
+    duplicate_addresses = [
+        str(address)
+        for address, count in collections.Counter(addresses).items()
+        if count > 1
+    ]
+    if duplicate_addresses:
+        if len(duplicate_addresses) == 1:
+            msg = "Duplicate address"
+        else:
+            msg = "Duplicate addresses"
+        raise cv.Invalid("{}: {}.".format(msg, ", ".join(duplicate_addresses)))
+    return value
+
+
+ADDRESS_SCHEMA = cv.All(
+    cv.Schema(
+        {
+            cv.Optional(CONF_ADDRESS, default=0): cv.hex_int_range(min=0, max=9999),
+            cv.Optional(CONF_ADDRESS_TYPE, default="16bit"): cv.enum(ADRESS_TYPE),
+        }
+    ),
+)
+
+CONFIG_SCHEMA = cv.All(
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(ModbusSensor),
+            cv.GenerateID(CONF_MODBUS_ID): cv.use_id(modbus.Modbus),
+            cv.Optional(CONF_DEVICE_ID, default=1): cv.hex_int_range(min=1, max=247),
+            cv.Optional(CONF_FUNCTION, default="read_input_registers"): cv.enum(
+                READ_FUNCTION
+            ),
+            cv.Required(CONF_ADDRESSES): cv.All(
+                cv.ensure_list(ADDRESS_SCHEMA),
+                cv.Length(min=1),
+                validate_addresses,
+            ),
+        }
+    ).extend(cv.polling_component_schema("60s")),
+)
 
 
 def to_code(config):
--- tests/component_tests/modbus/test_sensor.py
+++ tests/component_tests/modbus/test_sensor.py
@@ -2,7 +2,7 @@
 
 import pytest
 import esphome.config_validation as cv
-from esphome.components.modbus.sensor import CONFIG_SCHEMA
+from esphome.components.modbus.sensor import ADDRESS_SCHEMA, CONFIG_SCHEMA
 
 
 def test_setup_default(generate_main):
@@ -25,7 +25,7 @@ def test_setup_custom(generate_main):
 
 
 def test_default():
-    config = dict()
+    config = dict(addresses=[dict()])
     validated = CONFIG_SCHEMA(config)
     assert validated["modbus_id"].is_declaration == False
     assert validated["modbus_id"].is_manual == False
@@ -34,26 +34,26 @@ def test_default():
 
 
 def test_modbus_id():
-    config = dict(modbus_id="my_modbus")
+    config = dict(modbus_id="my_modbus", addresses=[dict()])
     validated = CONFIG_SCHEMA(config)
     assert validated["modbus_id"].is_declaration == False
     assert validated["modbus_id"].is_manual == True
 
 
 def test_device_update_interval():
-    config = dict(update_interval="5000ms")
+    config = dict(update_interval="5000ms", addresses=[dict()])
     validated = CONFIG_SCHEMA(config)
     assert validated["update_interval"].milliseconds == 5000
 
 
 def test_device_id_default():
-    config = dict(device_id=9)
+    config = dict(device_id=9, addresses=[dict()])
     validated = CONFIG_SCHEMA(config)
     assert validated["device_id"] == 9
 
 
 def test_device_id_too_low():
-    config = dict(device_id=0)
+    config = dict(device_id=0, addresses=[dict()])
     with pytest.raises(cv.MultipleInvalid) as excinfo:
         CONFIG_SCHEMA(config)
     assert excinfo.value.msg == "value must be at least 1"
@@ -61,7 +61,7 @@ def test_device_id_too_low():
 
 
 def test_device_id_too_high():
-    config = dict(device_id=248)
+    config = dict(device_id=248, addresses=[dict()])
     with pytest.raises(cv.MultipleInvalid) as excinfo:
         CONFIG_SCHEMA(config)
     assert excinfo.value.msg == "value must be at most 247"
@@ -69,13 +69,13 @@ def test_device_id_too_high():
 
 
 def test_function():
-    config = dict(function="read_coils")
+    config = dict(function="read_coils", addresses=[dict()])
     validated = CONFIG_SCHEMA(config)
     assert validated["function"] == "read_coils"
 
 
 def test_device_function_ko():
-    config = dict(function="unsupported_function")
+    config = dict(function="unsupported_function", addresses=[dict()])
     with pytest.raises(cv.MultipleInvalid) as excinfo:
         CONFIG_SCHEMA(config)
     assert (
@@ -83,3 +83,68 @@ def test_device_function_ko():
         == "Unknown value 'unsupported_function', valid options are 'read_coils', 'read_discrete_inputs', 'read_holding_registers', 'read_input_registers'."
     )
     assert excinfo.value.path == ["function"]
+
+
+def test_duplicate_address_1():
+    config = dict(
+        addresses=[
+            dict(address=1),
+            dict(address=1),
+        ]
+    )
+    with pytest.raises(cv.MultipleInvalid) as excinfo:
+        CONFIG_SCHEMA(config)
+    assert excinfo.value.msg == "Duplicate address: 0x01."
+    assert excinfo.value.path == ["addresses"]
+
+
+def test_duplicate_address_2():
+    config = dict(
+        addresses=[
+            dict(address=1),
+            dict(address=5),
+            dict(address=5),
+            dict(address=8),
+            dict(address=8),
+            dict(address=8),
+        ]
+    )
+    with pytest.raises(cv.MultipleInvalid) as excinfo:
+        CONFIG_SCHEMA(config)
+    assert excinfo.value.msg == "Duplicate addresses: 0x05, 0x08."
+    assert excinfo.value.path == ["addresses"]
+
+
+def test_address_default():
+    config = dict()
+    validated = ADDRESS_SCHEMA(config)
+    assert validated["address"] == 0
+
+
+def test_address_custom():
+    config = dict(address=0x09)
+    validated = ADDRESS_SCHEMA(config)
+    assert validated["address"] == 9
+
+
+def test_address_type_default():
+    config = dict()
+    validated = ADDRESS_SCHEMA(config)
+    assert validated["address_type"] == "16bit"
+
+
+def test_address_type_custom():
+    config = dict(address_type="32bit")
+    validated = ADDRESS_SCHEMA(config)
+    assert validated["address_type"] == "32bit"
+
+
+def test_address_type_ko():
+    config = dict(address_type="unsupported_address_type")
+    with pytest.raises(cv.MultipleInvalid) as excinfo:
+        ADDRESS_SCHEMA(config)
+    assert (
+        excinfo.value.msg
+        == "Unknown value 'unsupported_address_type', valid options are '16bit', '32bit'."
+    )
+    assert excinfo.value.path == ["address_type"]
